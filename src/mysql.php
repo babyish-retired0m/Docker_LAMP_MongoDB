2f55dc5c13131e62280e07f684bf4fb96170d30d
John Doe <97737897+babyish-retired0m@users.noreply.github.com>
2022-11-01 15:41:03 +0200

    repo sync

---
+++ src/mysql.php
@@ -0,0 +1,23 @@
+<?php
+	$hostname = 'mysql';
+	$dbname = 'helloworld';
+	$username = 'helloworld';
+	$password = 'helloworld';
+
+	try {
+		$conn = new PDO("mysql:host=$hostname;dbname=$dbname,$username, $password");
+
+		// Configure PDO error mode
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		echo 'Connected successfully';
+	}
+	catch( PDOException $e) {
+		echo 'Failed to connect: ' . $e->getMessage();
+	}
+
+	// Perform database operation
+
+	// Close the connection
+	$conn = null;
+?>
